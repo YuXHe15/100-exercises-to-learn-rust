37b66ce16386c1592ea1eec389bc64a3faa668b2
Yuxiang <yuxianghe61@gmail.com>
2024-12-25 15:39:57 +0100

    progress-20241225-3

--- exercises/05_ticket_v2/12_thiserror/src/lib.rs
+++ exercises/05_ticket_v2/12_thiserror/src/lib.rs
@@ -2,12 +2,16 @@
 //   We've changed the enum variants to be more specific, thus removing the need for storing
 //   a `String` field into each variant.
 //   You'll also have to add `thiserror` as a dependency in the `Cargo.toml` file.
-
+#[derive(thiserror::Error, Debug)]
 enum TicketNewError {
-    TitleCannotBeEmpty,
-    TitleTooLong,
-    DescriptionCannotBeEmpty,
-    DescriptionTooLong,
+    #[error("{0}")]
+    TitleCannotBeEmpty(String),
+    #[error("{0}")]
+    TitleTooLong(String),
+    #[error("{0}")]
+    DescriptionCannotBeEmpty(String),
+    #[error("{0}")]
+    DescriptionTooLong(String),
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -31,16 +35,16 @@ impl Ticket {
         status: Status,
     ) -> Result<Ticket, TicketNewError> {
         if title.is_empty() {
-            return Err(TicketNewError::TitleCannotBeEmpty);
+            return Err(TicketNewError::TitleCannotBeEmpty("Title cannot be empty".into()));
         }
         if title.len() > 50 {
-            return Err(TicketNewError::TitleTooLong);
+            return Err(TicketNewError::TitleTooLong("Title cannot be longer than 50 bytes".into()));
         }
         if description.is_empty() {
-            return Err(TicketNewError::DescriptionCannotBeEmpty);
+            return Err(TicketNewError::DescriptionCannotBeEmpty("Description cannot be empty".into()));
         }
         if description.len() > 500 {
-            return Err(TicketNewError::DescriptionTooLong);
+            return Err(TicketNewError::DescriptionTooLong("Description cannot be longer than 500 bytes".into()));
         }
 
         Ok(Ticket {
